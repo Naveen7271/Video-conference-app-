a0c93a149e218093f34fdce5514f9ecc2da4310b
Naveen <naveenn7271@gmail.com>
2025-12-30 20:16:55 +0530

    refactor: remove allowedDevOrigins from Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: [
-    'http://192.168.31.25:3000',
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-  ],
 };
 
 export default nextConfig;
